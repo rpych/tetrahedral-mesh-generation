626bb59b2603cc44c3bd27bcd6f7bdcba545bf2f
Andrzej97 <szafand@gmail.com>
2020-07-23 23:19:12 +0200

    Added second testing tetrahedra in Main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -17,6 +17,13 @@ public class Main {
         tetrahedra = transformator.transform();
         MatlabVisualizer matlabVisualizer = new MatlabVisualizer(tetrahedra, "vis");
         matlabVisualizer.saveCode();
+        
+        ModelGraph tetrahedra2 = generateTetrahedra2();
+        Transformator transformator2 = new Transformator(tetrahedra2);
+        tetrahedra2 = transformator2.transform();
+        MatlabVisualizer matlabVisualizer2 = new MatlabVisualizer(tetrahedra2, "vis2");
+        matlabVisualizer2.saveCode();
+        
         System.out.println("Program ended successfully");
     }
     
@@ -28,6 +35,30 @@ public class Main {
         nodes.add(graph.insertVertexAutoNamed(new Coordinates(0.5, 0.2, 0.0)));
         nodes.add(graph.insertVertexAutoNamed(new Coordinates(1.0, 0.0, 0.0)));
         nodes.add(graph.insertVertexAutoNamed(new Coordinates(0.0, 0.0, 0.0)));
+        
+        graph.insertEdgeAutoNamed(nodes.get(0), nodes.get(1), true);
+        graph.insertEdgeAutoNamed(nodes.get(1), nodes.get(2), true);
+        graph.insertEdgeAutoNamed(nodes.get(2), nodes.get(0), true);
+        graph.insertEdgeAutoNamed(nodes.get(0), nodes.get(3), true);
+        graph.insertEdgeAutoNamed(nodes.get(1), nodes.get(3), true);
+        graph.insertEdgeAutoNamed(nodes.get(2), nodes.get(3), true);
+
+        graph.insertFaceAutoNamed(nodes.get(0), nodes.get(1), nodes.get(2));
+        graph.insertFaceAutoNamed(nodes.get(0), nodes.get(1), nodes.get(3));
+        graph.insertFaceAutoNamed(nodes.get(1), nodes.get(2), nodes.get(3)).setR(true);;
+        graph.insertFaceAutoNamed(nodes.get(2), nodes.get(0), nodes.get(3));
+
+        return graph;
+    }
+    
+    private static ModelGraph generateTetrahedra2() {
+        ModelGraph graph = new ModelGraph("Graph2");
+
+        List<Vertex> nodes = new ArrayList<>();
+        nodes.add(graph.insertVertexAutoNamed(new Coordinates(0.64, 0.50, 0.80)));
+        nodes.add(graph.insertVertexAutoNamed(new Coordinates(0.33, 0.2, 1.0)));
+        nodes.add(graph.insertVertexAutoNamed(new Coordinates(1.0, 2.0, 1.4)));
+        nodes.add(graph.insertVertexAutoNamed(new Coordinates(2.0, 0.4, 1.5)));
         
         graph.insertEdgeAutoNamed(nodes.get(0), nodes.get(1), true);
         graph.insertEdgeAutoNamed(nodes.get(1), nodes.get(2), true);
