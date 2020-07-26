9700432853197460d1722c9bba9d7aa510112800
Andrzej97 <szafand@gmail.com>
2020-07-26 15:03:34 +0200

    Change Andrzej testing method to Rafal

--- src/main/java/tests/TransformatorTests.java
+++ src/main/java/tests/TransformatorTests.java
@@ -1,45 +1,54 @@
-/*package tests;
+package tests;
+
+import controller.ITransformator;
+import controller.Transformator;
+import controller.TransformatorForTests;
+import model.*;
+import org.javatuples.Triplet;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import visualization.MatlabVisualizer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
+class GraphTransformatorTest {
 
-import org.junit.jupiter.api.Test;
+    private ModelGraph inputGraph;
+    private Transformator transformator; //for inputGraph
+    private ModelGraph expectedGraph;
+    private TransformatorForTests transformatorForTests; //for expectedGraph
 
-import controller.Transformator;
-import model.Coordinates;
-import model.FaceNode;
-import model.GraphEdge;
-import model.ModelGraph;
-import model.Vertex;
+    static ModelGraph makeGraphTransformation(ITransformator transformator){
+        ModelGraph tetrahedra = transformator.transform();
+        //MatlabVisualizer matlabVisualizer = new MatlabVisualizer(tetrahedra, "vis");
+        //matlabVisualizer.saveCode();
+        return tetrahedra;
+    }
 
-class TransformatorTests {
+    private void prepareInputGraph(){
+        this.inputGraph = generateTetrahedra("InGraph");
+        this.transformator = new Transformator(this.inputGraph);
+    }
 
-	@Test
-	void sampleTransformationTest() {
-		ModelGraph graph = createGraph1();
-		        
-        Transformator transformator = new Transformator(graph);
-        graph = transformator.transform();
-        
-        checkVertices1(graph);
-        // TODO:
-        // checkEdges1(graph);
-        checkFaces1(graph);
-	}
-	
-	ModelGraph createGraph1() {
-		ModelGraph graph = new ModelGraph("Graph");
+    private void prepareExpectedGraph(){
+        this.expectedGraph = generateTetrahedra("ExGraph");
+        this.transformatorForTests = new TransformatorForTests(this.expectedGraph);
+    }
+
+    private static ModelGraph generateTetrahedra(String name) {
+        ModelGraph graph = new ModelGraph(name);
 
         List<Vertex> nodes = new ArrayList<>();
         nodes.add(graph.insertVertexAutoNamed(new Coordinates(0.6, 0.40, 0.80)));
         nodes.add(graph.insertVertexAutoNamed(new Coordinates(0.5, 0.2, 0.0)));
         nodes.add(graph.insertVertexAutoNamed(new Coordinates(1.0, 0.0, 0.0)));
         nodes.add(graph.insertVertexAutoNamed(new Coordinates(0.0, 0.0, 0.0)));
-        
+
         graph.insertEdgeAutoNamed(nodes.get(0), nodes.get(1), true);
         graph.insertEdgeAutoNamed(nodes.get(1), nodes.get(2), true);
         graph.insertEdgeAutoNamed(nodes.get(2), nodes.get(0), true);
@@ -49,123 +58,115 @@ class TransformatorTests {
 
         graph.insertFaceAutoNamed(nodes.get(0), nodes.get(1), nodes.get(2));
         graph.insertFaceAutoNamed(nodes.get(0), nodes.get(1), nodes.get(3));
-        graph.insertFaceAutoNamed(nodes.get(1), nodes.get(2), nodes.get(3)).setR(true);;
+        graph.insertFaceAutoNamed(nodes.get(1), nodes.get(2), nodes.get(3)).setR(true);
         graph.insertFaceAutoNamed(nodes.get(2), nodes.get(0), nodes.get(3));
 
         return graph;
-	}
-	
-	void checkVertices1(ModelGraph graph) {
-        Collection<Vertex> vertices = graph.getVertices();
-        assertEquals(6, vertices.size());
-        
-        Optional<Vertex> v;
-        
-        v = graph.getVertex("V_0,60_0,40_0,80");
-        assertTrue(v.isPresent());
-        assertEquals(0.60, v.get().getXCoordinate(), 0.01);
-        assertEquals(0.40, v.get().getYCoordinate(), 0.01);
-        assertEquals(0.80, v.get().getZCoordinate(), 0.01);
-        
-        v = graph.getVertex("V_0,50_0,20_0,00");
-        assertTrue(v.isPresent());
-        assertEquals(0.50, v.get().getXCoordinate(), 0.01);
-        assertEquals(0.20, v.get().getYCoordinate(), 0.01);
-        assertEquals(0.00, v.get().getZCoordinate(), 0.01);
+    }
 
-        v = graph.getVertex("V_1,00_0,00_0,00");
-        assertTrue(v.isPresent());
-        assertEquals(1.00, v.get().getXCoordinate(), 0.01);
-        assertEquals(0.00, v.get().getYCoordinate(), 0.01);
-        assertEquals(0.00, v.get().getZCoordinate(), 0.01);
-        
-        v = graph.getVertex("V_0,00_0,00_0,00");
-        assertTrue(v.isPresent());
-        assertEquals(0.00, v.get().getXCoordinate(), 0.01);
-        assertEquals(0.00, v.get().getYCoordinate(), 0.01);
-        assertEquals(0.00, v.get().getZCoordinate(), 0.01);
-        
-        v = graph.getVertex("V_0,50_0,00_0,00");
-        assertTrue(v.isPresent());
-        assertEquals(0.50, v.get().getXCoordinate(), 0.01);
-        assertEquals(0.00, v.get().getYCoordinate(), 0.01);
-        assertEquals(0.00, v.get().getZCoordinate(), 0.01);
-                
-        v = graph.getVertex("V_0,30_0,20_0,40");
-        assertTrue(v.isPresent());
-        assertEquals(0.30, v.get().getXCoordinate(), 0.01);
-        assertEquals(0.20, v.get().getYCoordinate(), 0.01);
-        assertEquals(0.40, v.get().getZCoordinate(), 0.01);
-	}
-	
-	void checkEdges1(ModelGraph graph) {
-		Collection<GraphEdge> edges = graph.getEdges();
-		for(GraphEdge edge : edges) {
-			System.out.println(edge.getId());
-		}
-	}
-	
-	void checkFaces1(ModelGraph graph) {
-		Collection<FaceNode> faces = graph.getFaces();
-        assertEquals(8, faces.size());
-      
-        Optional<FaceNode> f;
-        
-        f = graph.getFace("F_0,27_0,07_0,13");
-        assertTrue(f.isPresent());
-        assertEquals(0.27, f.get().getXCoordinate(), 0.01);
-        assertEquals(0.07, f.get().getYCoordinate(), 0.01);
-        assertEquals(0.13, f.get().getZCoordinate(), 0.01);
-        assertFalse(f.get().isR());
-        
-        f = graph.getFace("F_0,60_0,07_0,13");
-        assertTrue(f.isPresent());
-        assertEquals(0.60, f.get().getXCoordinate(), 0.01);
-        assertEquals(0.07, f.get().getYCoordinate(), 0.01);
-        assertEquals(0.13, f.get().getZCoordinate(), 0.01);
-        assertFalse(f.get().isR());
+    private Boolean isVertexPresentInInputGraph(Vertex v, ModelGraph transformed){
+        Optional<Vertex> optVer =  transformed.getVertex(v.getId());
+        return optVer.isPresent() && (v.getCoordinates()).equals(optVer.get().getCoordinates());
+    }
 
-        f = graph.getFace("F_0,63_0,20_0,40");
-        assertTrue(f.isPresent());
-        assertEquals(0.63, f.get().getXCoordinate(), 0.01);
-        assertEquals(0.20, f.get().getYCoordinate(), 0.01);
-        assertEquals(0.40, f.get().getZCoordinate(), 0.01);
-        assertFalse(f.get().isR());
+    private Boolean isEdgePresentInInputGraph(GraphEdge e, ModelGraph transformed){
+        Optional<GraphEdge> optEdge = transformed.getEdgeById(e.getId());
+        return optEdge.isPresent() && transformed.isEdgeBetween(optEdge.get().getEdgeNodes().getValue0(),
+                                                                optEdge.get().getEdgeNodes().getValue1());
+    }
 
-        f = graph.getFace("F_0,33_0,07_0,00");
-        assertTrue(f.isPresent());
-        assertEquals(0.33, f.get().getXCoordinate(), 0.01);
-        assertEquals(0.07, f.get().getYCoordinate(), 0.01);
-        assertEquals(0.00, f.get().getZCoordinate(), 0.01);
-        assertFalse(f.get().isR());
+    private Boolean isFacePresentInInputGraph(FaceNode f, ModelGraph transformed){
+        Optional<FaceNode> optFace = transformed.getFace(f.getId());
+        if(!optFace.isPresent()) { return false; }
+        Triplet<Vertex, Vertex, Vertex> inputTriangle = optFace.get().getTriangle();
+        Triplet<Vertex, Vertex, Vertex> expectedTriangle = f.getTriangle();
+        return (inputTriangle.getValue0().getCoordinates()).equals(expectedTriangle.getValue0().getCoordinates()) &&
+                (inputTriangle.getValue1().getCoordinates()).equals(expectedTriangle.getValue1().getCoordinates()) &&
+                (inputTriangle.getValue2().getCoordinates()).equals(expectedTriangle.getValue2().getCoordinates()) &&
+                f.getCoordinates().equals(optFace.get().getCoordinates()); //this line for checking face representative node/vertex
+    }
 
-        f = graph.getFace("F_0,67_0,07_0,00");
-        assertTrue(f.isPresent());
-        assertEquals(0.67, f.get().getXCoordinate(), 0.01);
-        assertEquals(0.07, f.get().getYCoordinate(), 0.01);
-        assertEquals(0.00, f.get().getZCoordinate(), 0.01);
-        assertFalse(f.get().isR());
-        
-        f = graph.getFace("F_0,27_0,13_0,13");
-        assertTrue(f.isPresent());
-        assertEquals(0.27, f.get().getXCoordinate(), 0.01);
-        assertEquals(0.13, f.get().getYCoordinate(), 0.01);
-        assertEquals(0.13, f.get().getZCoordinate(), 0.01);
-        assertFalse(f.get().isR());
-        
-        f = graph.getFace("F_0,47_0,27_0,40");
-        assertTrue(f.isPresent());
-        assertEquals(0.47, f.get().getXCoordinate(), 0.01);
-        assertEquals(0.27, f.get().getYCoordinate(), 0.01);
-        assertEquals(0.40, f.get().getZCoordinate(), 0.01);
-        assertFalse(f.get().isR());
-        
-        f = graph.getFace("F_0,70_0,20_0,27");
-        assertTrue(f.isPresent());
-        assertEquals(0.70, f.get().getXCoordinate(), 0.01);
-        assertEquals(0.20, f.get().getYCoordinate(), 0.01);
-        assertEquals(0.27, f.get().getZCoordinate(), 0.01);
-        assertFalse(f.get().isR());
-	}
+    @BeforeEach
+    void setupGraphsAndTransformators(){
+        prepareInputGraph();
+        prepareExpectedGraph();
+    }
+
+    @Test
+    void simpletest(){
+       assertEquals(1, 1);
+    }
+
+    @Test
+    void checkEqualNumOfVertexesInBothGraphs(){
+        ModelGraph transformedExpGraph = makeGraphTransformation(this.transformatorForTests);
+
+        ModelGraph transformedInputGraph = makeGraphTransformation(this.transformator);
+
+        assertEquals(transformedExpGraph.getVerticesNum(), transformedInputGraph.getVerticesNum());
+    }
+
+    @Test
+    void checkEqualNumOfEdgesInBothGraphs(){
+        ModelGraph transformedExpGraph = makeGraphTransformation(this.transformatorForTests);
+
+        ModelGraph transformedInputGraph = makeGraphTransformation(this.transformator);
+
+        assertEquals(transformedExpGraph.getEdgesNum(), transformedInputGraph.getEdgesNum());
+    }
+
+    @Test
+    void checkEqualNumOfFacesInBothGraphs(){
+        ModelGraph transformedExpGraph = makeGraphTransformation(this.transformatorForTests);
+
+        ModelGraph transformedInputGraph = makeGraphTransformation(this.transformator);
+
+        assertEquals(transformedExpGraph.getFacesNum(), transformedInputGraph.getFacesNum());
+    }
+
+    @Test
+    void checkSameVerticesInBothGraphs(){
+        ModelGraph transformedExpGraph = makeGraphTransformation(this.transformatorForTests);
+
+        ModelGraph transformedInputGraph = makeGraphTransformation(this.transformator);
+
+        List<Boolean> results = transformedExpGraph.getVertices().stream().map(v -> isVertexPresentInInputGraph(v, transformedInputGraph))
+                                                                          .collect(Collectors.toList());
+
+        for(Boolean r: results) {
+            assertTrue(r);
+        }
+    }
+
+    @Test
+    void checkSameEdgesInBothGraphs(){
+        ModelGraph transformedExpGraph = makeGraphTransformation(this.transformatorForTests);
+        /*List<Vertex> nodes = new ArrayList<>();
+        nodes.add(transformedExpGraph.insertVertexAutoNamed(new Coordinates(-0.6, -0.40, -0.80)));
+        nodes.add(transformedExpGraph.insertVertexAutoNamed(new Coordinates(0.3, 0.1, 0.0)));
+        transformedExpGraph.insertEdgeAutoNamed(nodes.get(0), nodes.get(1), true);*/
+
+        ModelGraph transformedInputGraph = makeGraphTransformation(this.transformator);
+
+        List<Boolean> results = transformedExpGraph.getEdges().stream().map(e -> isEdgePresentInInputGraph(e, transformedInputGraph))
+                .collect(Collectors.toList());
+
+        for(Boolean r: results) {
+            assertTrue(r);
+        }
+    }
+
+    @Test
+    void checkSameFacesInBothGraphs(){
+        ModelGraph transformedExpGraph = makeGraphTransformation(this.transformatorForTests);
+
+        ModelGraph transformedInputGraph = makeGraphTransformation(this.transformator);
+
+        List<Boolean> results = transformedExpGraph.getFaces().stream().map(f -> isFacePresentInInputGraph(f, transformedInputGraph))
+                .collect(Collectors.toList());
+
+        for(Boolean r: results) {
+            assertTrue(r);
+        }
 }
-*/
+}
