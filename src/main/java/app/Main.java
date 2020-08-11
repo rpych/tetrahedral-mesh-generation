4813326b3495bbfcf88e4033ed0bc59b49bc4b65
Andrzej97 <szafand@gmail.com>
2020-08-11 18:05:00 +0200

    Added generating startaing cuboid made of 5 tetrahedrals

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -24,6 +24,12 @@ public class Main {
         MatlabVisualizer matlabVisualizer2 = new MatlabVisualizer(tetrahedra2, "vis2");
         matlabVisualizer2.saveCode();
         
+        ModelGraph cuboid = generateCuboid(new Coordinates(0.0, 0.0, 0.0), 2.0, 1.0, 1.0);
+//        Transformator transformator3 = new Transformator(cuboid);
+//        cuboid = transformator3.transform();
+        MatlabVisualizer matlabVisualizer3 = new MatlabVisualizer(cuboid, "vis3");
+        matlabVisualizer3.saveCode();
+        
         System.out.println("Program ended successfully");
     }
     
@@ -79,4 +85,61 @@ public class Main {
         return graph;
     }
 
+    private static ModelGraph generateCuboid(Coordinates basePoint, double x, double y, double z) {
+        ModelGraph graph = new ModelGraph("Graph");
+
+        List<Vertex> nodes = new ArrayList<>();
+        nodes.add(graph.insertVertexAutoNamed(new Coordinates(basePoint.getX(), basePoint.getY(), basePoint.getZ())));
+        nodes.add(graph.insertVertexAutoNamed(new Coordinates(basePoint.getX() + x, basePoint.getY(), basePoint.getZ())));
+        nodes.add(graph.insertVertexAutoNamed(new Coordinates(basePoint.getX() + x, basePoint.getY() + y, basePoint.getZ())));
+        nodes.add(graph.insertVertexAutoNamed(new Coordinates(basePoint.getX(), basePoint.getY() + y, basePoint.getZ())));
+        nodes.add(graph.insertVertexAutoNamed(new Coordinates(basePoint.getX(), basePoint.getY(), basePoint.getZ() + z)));
+        nodes.add(graph.insertVertexAutoNamed(new Coordinates(basePoint.getX() + x, basePoint.getY(), basePoint.getZ() + z)));
+        nodes.add(graph.insertVertexAutoNamed(new Coordinates(basePoint.getX() + x, basePoint.getY() + y, basePoint.getZ() + z)));
+        nodes.add(graph.insertVertexAutoNamed(new Coordinates(basePoint.getX(), basePoint.getY() + y, basePoint.getZ() + z)));
+        
+        graph.insertEdgeAutoNamed(nodes.get(0), nodes.get(1), true);
+        graph.insertEdgeAutoNamed(nodes.get(0), nodes.get(3), true);
+        graph.insertEdgeAutoNamed(nodes.get(0), nodes.get(4), true);
+        graph.insertEdgeAutoNamed(nodes.get(1), nodes.get(2), true);
+        graph.insertEdgeAutoNamed(nodes.get(1), nodes.get(3), true);
+        graph.insertEdgeAutoNamed(nodes.get(1), nodes.get(4), true);
+        graph.insertEdgeAutoNamed(nodes.get(1), nodes.get(5), true);
+        graph.insertEdgeAutoNamed(nodes.get(1), nodes.get(6), true);
+        graph.insertEdgeAutoNamed(nodes.get(2), nodes.get(3), true);
+        graph.insertEdgeAutoNamed(nodes.get(2), nodes.get(6), true);
+        graph.insertEdgeAutoNamed(nodes.get(3), nodes.get(4), true);
+        graph.insertEdgeAutoNamed(nodes.get(3), nodes.get(6), true);
+        graph.insertEdgeAutoNamed(nodes.get(3), nodes.get(7), true);
+        graph.insertEdgeAutoNamed(nodes.get(4), nodes.get(5), true);
+        graph.insertEdgeAutoNamed(nodes.get(4), nodes.get(6), true);
+        graph.insertEdgeAutoNamed(nodes.get(4), nodes.get(7), true);
+        graph.insertEdgeAutoNamed(nodes.get(5), nodes.get(6), true);
+        graph.insertEdgeAutoNamed(nodes.get(6), nodes.get(7), true);
+
+        graph.insertFaceAutoNamed(nodes.get(0), nodes.get(1), nodes.get(3)).setR(true);;
+        graph.insertFaceAutoNamed(nodes.get(0), nodes.get(1), nodes.get(4));
+        graph.insertFaceAutoNamed(nodes.get(0), nodes.get(3), nodes.get(4));
+        graph.insertFaceAutoNamed(nodes.get(1), nodes.get(2), nodes.get(3));
+        graph.insertFaceAutoNamed(nodes.get(1), nodes.get(2), nodes.get(6));
+        graph.insertFaceAutoNamed(nodes.get(1), nodes.get(3), nodes.get(4));
+        graph.insertFaceAutoNamed(nodes.get(1), nodes.get(3), nodes.get(6));
+        graph.insertFaceAutoNamed(nodes.get(1), nodes.get(4), nodes.get(6));
+        graph.insertFaceAutoNamed(nodes.get(1), nodes.get(4), nodes.get(5));
+        graph.insertFaceAutoNamed(nodes.get(1), nodes.get(5), nodes.get(6));
+        graph.insertFaceAutoNamed(nodes.get(2), nodes.get(3), nodes.get(6));
+        graph.insertFaceAutoNamed(nodes.get(3), nodes.get(4), nodes.get(6));
+        graph.insertFaceAutoNamed(nodes.get(3), nodes.get(4), nodes.get(7));
+        graph.insertFaceAutoNamed(nodes.get(3), nodes.get(6), nodes.get(7));
+        graph.insertFaceAutoNamed(nodes.get(4), nodes.get(5), nodes.get(6));
+        graph.insertFaceAutoNamed(nodes.get(4), nodes.get(6), nodes.get(7));
+
+        graph.insertInteriorNodeAutoNamed(nodes.get(0), nodes.get(1), nodes.get(3), nodes.get(4));
+        graph.insertInteriorNodeAutoNamed(nodes.get(1), nodes.get(2), nodes.get(3), nodes.get(6));
+        graph.insertInteriorNodeAutoNamed(nodes.get(1), nodes.get(3), nodes.get(4), nodes.get(6));
+        graph.insertInteriorNodeAutoNamed(nodes.get(1), nodes.get(4), nodes.get(5), nodes.get(6));
+        graph.insertInteriorNodeAutoNamed(nodes.get(3), nodes.get(4), nodes.get(6), nodes.get(7));
+
+        return graph;
+    }
 }
